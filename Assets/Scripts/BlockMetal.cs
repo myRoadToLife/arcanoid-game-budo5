d9f0d300a7bfa6ff82d81c6f6978b2ac4846b0ae
Andrey Alferov <alukard017@gmail.com>
2023-07-30 19:32:29 +0300

    Creating additional blocks and their logic

---
+++ Assets/Scripts/BlockMetal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Arkanoid
+{
+    public class BlockMetal : MonoBehaviour
+    {
+        #region Variables
+
+        private int _lives = 5;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ball"))
+            {
+                _lives--;
+                if (_lives == 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/BlockWood.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Arkanoid
+{
+    public class BlockWood : MonoBehaviour
+    {
+        #region Variables
+
+        private int _lives = 3;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ball"))
+            {
+                _lives--;
+                if (_lives == 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
